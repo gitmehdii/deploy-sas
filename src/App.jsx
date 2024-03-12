b039fee811825faf61e57a3aed1d41c9b4da36a3
MDI <mehdi.azouz@epita.fr>
2024-03-12 15:17:57 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './Petite.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
+import PageNotFound from "./pages/PageNotFound";
+
+import Navbar from "./pages/Navbar";
+import Footer from "./pages/Footer";
+
+function App() {
+  return (
+   <Router basename = {process.env.PUBLIC_URL}>
+   <Navbar />
+       <Routes>
+           <Route path="/" element = {<Home />} />
+           <Route path="/products" element = {<Products />} />
+           <Route path="/contact" element = {<Contact />} />
+           <Route path="*" element = { <PageNotFound /> } />            
+       </Routes>
+   <Footer />
+   </Router>
+  );
+}
+
+export default App;
