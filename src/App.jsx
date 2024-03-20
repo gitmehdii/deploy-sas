6791003bca7517459f61b060f6132b50cfb6a5b3
MDI <mehdi.azouz@epita.fr>
2024-03-20 09:25:02 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,26 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Contact from "./pages/Contact";
 import PageNotFound from "./pages/PageNotFound";
-
 import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
 
 function App() {
-  return (
-   <Router>
-   <Navbar />
-       <Routes>
-           <Route path="/" element = {<Home />} />
-           <Route path="/products" element = {<Products />} />
-           <Route path="/contact" element = {<Contact />} />
-           <Route path="*" element = { <PageNotFound /> } />            
-       </Routes>
-   <Footer />
-   </Router>
-  );
+    return (
+        <Router >
+            <Navbar />
+            <main>
+                {/* Place non-global routes inside an HTML <main> tag */}
+                <Routes>
+                    {/* Use Home component for the root path */}
+                    <Route index element={<Home />} />
+                    <Route path="products" element={<Products />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+            </main>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
